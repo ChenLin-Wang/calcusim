d2b3300d35dbf26f00782d461f0fd3cac0ce8be2
JAD <jasondsouza2003@gmail.com>
2023-09-27 09:26:02 +0000

    V0.0.2.5 Added asset folder

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# Calcusim
-Just for practice collaboration
--- src/components/head.tsx
+++ src/components/head.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export default function Head(){
-    const [ title, setTitle ] = useState('Hello World');
+    const [ title, setTitle ] = useState<string>('Hello World');
     return <div className="fixed inset-x-0 h-10 bg-blue-900 flex">
         <button className="icon text-white flex-none w-10 text-center">&#xf0c9;</button>
         <span className="text-white text-center grow leading-10 mr-10">{title}</span>

1b72e00b12fc43c97578a2f694f4526ea173f063
JAD <jasondsouza2003@gmail.com>
2023-09-27 08:41:10 +0000

    V0.0.2.4 Added app and head component

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,3 +1,6 @@
+import Head from './head';
 export default function App() {
-    return <b>Hello World</b>
+    return <div className="fixed inset-x-0 inset-y-0 bg-slate-950">
+        <Head />
+    </div>
 }

e5f0bec49b2ae74785549a5bb38922bc9ed049a7
ChenLin Wang <wangchenlin2001@icloud.com>
2023-10-17 01:22:17 +0000

    V0.0.5.8 bug fix.

--- cpp_wasm/operations_cwrap.js
+++ cpp_wasm/operations_cwrap.js
@@ -1,14 +1,14 @@
-var sum = () => { Module.cwrap('addition', 'number', ['array', 'number']); }
-var prod = () => { Module.cwrap('multiplication', 'number', ['array', 'number']); }
-var sin = () => { Module.cwrap('sine', 'number', ['number', 'number']); }
-var cos = () => { Module.cwrap('cosine', 'number', ['number', 'number']); }
-var tan = () => { Module.cwrap('tangle', 'number', ['number', 'number']); }
-var asin = () => { Module.cwrap('asine', 'number', ['number', 'number']); }
-var acos = () => { Module.cwrap('acosine', 'number', ['number', 'number']); }
-var atan = () => { Module.cwrap('atangle', 'number', ['number', 'number']); }
-var exp = () => { Module.cwrap('exponentiation', ['number', 'number']); }
-var fac = () => { Module.cwrap('factorial', ['number']); }
-var log = () => { Module.cwrap('logarithm', ['number', 'number']); }
+var sum = () => { return Module.cwrap('addition', 'number', ['array', 'number']); }
+var prod = () => { return Module.cwrap('multiplication', 'number', ['array', 'number']); }
+var sin = () => { return Module.cwrap('sine', 'number', ['number', 'number']); }
+var cos = () => { return Module.cwrap('cosine', 'number', ['number', 'number']); }
+var tan = () => { return Module.cwrap('tangle', 'number', ['number', 'number']); }
+var asin = () => { return Module.cwrap('asine', 'number', ['number', 'number']); }
+var acos = () => { return Module.cwrap('acosine', 'number', ['number', 'number']); }
+var atan = () => { return Module.cwrap('atangle', 'number', ['number', 'number']); }
+var exp = () => { return Module.cwrap('exponentiation', ['number', 'number']); }
+var fac = () => { return Module.cwrap('factorial', ['number']); }
+var log = () => { return Module.cwrap('logarithm', ['number', 'number']); }
 
 var toTypedArray_Double = function(arr) {
     var buf = new ArrayBuffer(arr.length * 8);
